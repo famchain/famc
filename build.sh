08266fd538b34047ae64a950d32f1e93a2ef1c15
Christopher Gilliard <christopher.gilliard@gmail.com>
2026-04-06 11:23:10 -0700

    setup ignores

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# Bootstrap build. Each stage compiles the next from source using only the
+# previous stage's binary. No external tools other than QEMU.
+set -e
+run() {
+	(cat "$2"; printf '\004') | qemu-system-riscv32 \
+		-machine virt \
+		-cpu rv32i \
+		-nographic \
+		-bios none \
+		-device loader,file="$1",addr=0x80000000 \
+		-serial mon:stdio 2>/dev/null;
+}
+run fam0.seed src/fam0.fam0 > bin/fam0
+cmp ./bin/fam0 ./fam0.seed || { echo "binaries don't match!"; exit 1; }
+echo "Success!";
