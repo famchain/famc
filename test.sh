61f7548a105058fa05754df3e160b0ee33c5ff0b
Christopher Gilliard <christopher.gilliard@gmail.com>
2026-04-07 08:29:53 -0700

    stdlib

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-  (cat tests/test_long_jump.fam3; printf '\004') | \
-    qemu-system-riscv32 -machine virt -cpu rv32i -nographic -bios none \
-      -device loader,file=bin/fam3,addr=0x80000000 -serial mon:stdio 2>/dev/null > /tmp/test.bin
-  qemu-system-riscv32 -machine virt -cpu rv32i -nographic -bios none \
-    -device loader,file=/tmp/test.bin,addr=0x80000000 -serial mon:stdio 2>/dev/null 
