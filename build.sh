c9c0d14e78a6888b0a9d4697795ffed1805c9301
Christopher Gilliard <christopher.gilliard@gmail.com>
2026-04-09 00:07:04 -0700

    fam2

--- build.sh
+++ build.sh
@@ -13,6 +13,10 @@ run() {
 }
 run fam0.seed src/fam0.fam0 > bin/fam0
 cmp ./bin/fam0 ./fam0.seed || { echo "fam0: binaries don't match!"; exit 1; }
+echo "fam0: OK"
 run bin/fam0 src/fam1.fam0 > bin/fam1
+echo "fam1: OK ($(wc -c < bin/fam1) bytes)"
+run bin/fam1 src/fam2.fam1 > bin/fam2
+echo "fam2: OK ($(wc -c < bin/fam2) bytes)"
 
 echo "Success!";
