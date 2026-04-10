c6b3ad28ebf1d98134746ea2fa8cb9a74faef1a6
Christopher Gilliard <christopher.gilliard@gmail.com>
2026-04-10 00:28:45 -0700

    build and test on newer qemu

--- build.sh
+++ build.sh
@@ -5,11 +5,10 @@ set -e
 run() {
 	(cat "$2"; printf '\004') | qemu-system-riscv32 \
 		-machine virt \
-		-cpu rv32i \
+		-cpu rv32,m=false,a=false,f=false,d=false,c=false,zawrs=false,zfa=false,zfh=false,zfhmin=false,zcb=false,zcd=false,zcf=false,zcmp=false,zcmt=false,zicsr=false,zifencei=false \
 		-nographic \
 		-bios none \
-		-device loader,file="$1",addr=0x80000000 \
-		-serial mon:stdio 2>/dev/null
+		-device loader,file="$1",addr=0x80000000
 }
 run fam0.seed src/fam0.fam0 > bin/fam0
 cmp ./bin/fam0 ./fam0.seed || { echo "fam0: binaries don't match!"; exit 1; }
