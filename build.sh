25d2b643504739b2e48bf5cfa593a9ef61b3834a
Christopher Gilliard <christopher.gilliard@gmail.com>
2026-04-10 15:29:27 -0700

    clean out

--- build.sh
+++ build.sh
@@ -5,7 +5,10 @@ set -e
 run() {
 	(cat "$2"; printf '\004') | qemu-system-riscv32 \
 		-machine virt \
-		-cpu rv32,m=false,a=false,f=false,d=false,c=false,zawrs=false,zfa=false,zfh=false,zfhmin=false,zcb=false,zcd=false,zcf=false,zcmp=false,zcmt=false,zicsr=false,zifencei=false \
+		-cpu rv32,\
+m=false,a=false,f=false,d=false,c=false,zawrs=false,zfa=false,\
+zfh=false,zfhmin=false,zcb=false,zcd=false,zcf=false,zcmp=false,\
+zcmt=false,zicsr=false,zifencei=false \
 		-nographic \
 		-bios none \
 		-device loader,file="$1",addr=0x80000000
@@ -15,6 +18,5 @@ cmp ./bin/fam0 ./fam0.seed || { echo "fam0: binaries don't match!"; exit 1; }
 run bin/fam0 src/fam1.fam0 > bin/fam1
 run bin/fam1 src/fam2.fam1 > bin/fam2
 run bin/fam2 src/fam3.fam2 > bin/fam3
-run bin/fam3 src/famc.fam3 > bin/famc
 
 echo "Success!";
